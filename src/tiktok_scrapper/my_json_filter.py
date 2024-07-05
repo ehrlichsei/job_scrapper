11a50e311599371847aa23cb16ae7021f1129e7f
yuli.fang <fangnuaa@gmail.com>
2024-07-06 00:20:00 +0200

    extract values

--- src/tiktok_scrapper/my_json_filter.py
+++ src/tiktok_scrapper/my_json_filter.py
@@ -1,16 +1,20 @@
 import json
 from pathlib import Path
 
+EXTRACT_KEY = 'webVideoUrl'
+INPUT_PATH = 'data/tiktok_videos.json'
+OUTPUT_PATH = 'output/tiktok_webVideoUrl.txt'
+
 # 定义输入和输出文件路径
-input_file_path = Path('data/tiktok_videos.json')
-output_file_path = Path('output/tiktok_webVideoUrl.txt')
+input_file_path = Path(INPUT_PATH)
+output_file_path = Path(OUTPUT_PATH)
 
 # 读取JSON文件内容
 with input_file_path.open('r', encoding='utf-8') as input_file:
     data = json.load(input_file)
 
 # 提取所有webVideoUrl的值
-web_video_urls = [item["webVideoUrl"] for item in data if "webVideoUrl" in item]
+web_video_urls = [item[EXTRACT_KEY] for item in data if EXTRACT_KEY in item]
 
 # 将提取的webVideoUrl值写入文本文件
 with output_file_path.open('w', encoding='utf-8') as output_file:
